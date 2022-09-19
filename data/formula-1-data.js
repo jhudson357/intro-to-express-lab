cdc4bf8988a7db403d7944597d3d038426a1a229
James Hudson <jmh357@comcast.net>
2022-09-19 17:41:22 -0500

    add data directory and file

---
+++ data/formula-1-data.js
@@ -0,0 +1,26 @@
+const formula1 = [
+  {name: 'Max Verstappen', points: 335, winIn2022: true},
+  {name: 'Charles Leclerc', points: 219, winIn2022: true},
+  {name: 'Sergio Perez', points: 210, winIn2022: true},
+  {name: 'George Russell', points: 203, winIn2022: false},
+  {name: 'Carlos Sainz Jr.', points: 187, winIn2022: true},
+  {name: 'Lewis Hamilton', points: 168, winIn2022: false},
+  {name: 'Lando Norris', points: 88, winIn2022: false},
+  {name: 'Esteban Ocon', points: 66, winIn2022: false},
+  {name: 'Fernando Alonso', points: 59, winIn2022: false},
+  {name: 'Valtteri Bottas', points: 46, winIn2022: false},
+  {name: 'Pierre Gasly', points: 22, winIn2022: false},
+  {name: 'Kevin Magnussen', points: 22, winIn2022: false},
+  {name: 'Sebastian Vettel', points: 20, winIn2022: false},
+  {name: 'Daniel Ricciardo', points: 19, winIn2022: false},
+  {name: 'Mick Schumacher', points: 12, winIn2022: false},
+  {name: 'Yuki Tsunoda', points: 11, winIn2022: false},
+  {name: 'Zhou Guanyu', points: 6, winIn2022: false},
+  {name: 'Lance Stroll', points: 5, winIn2022: false},
+  {name: 'Alex ALbon', points: 4, winIn2022: false},
+  {name: 'Nicholas Latifi', points: 0, winIn2022: false},
+]
+
+export {
+  formula1
+}
