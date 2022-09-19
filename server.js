23b4c4cea7c5109a5478263f2d8a95c8088cde3a
James Hudson <jmh357@comcast.net>
2022-09-19 17:05:44 -0500

    write basic structure of express app

---
+++ server.js
@@ -0,0 +1,16 @@
+// import modules
+import express from 'express'
+
+// create express app
+const app = express()
+
+// configure the app (app.set)
+
+// mount middleware (app.use)
+
+// mount routes
+
+// tell teh app to listen on port 3000
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
