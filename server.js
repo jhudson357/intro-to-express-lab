3a72d2d016e23f70364a24df85cb75a566bccf77
James Hudson <jmh357@comcast.net>
2022-09-19 17:51:23 -0500

    create new formula1 directory in views directory. create index.ejs file in formula 1 directory. add render method for index file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // import modules
 import express from 'express'
+import { formula1 } from './data/formula-1-data.js';
 
 // create express app
 const app = express()
@@ -11,12 +12,18 @@ app.set('view engine', 'ejs')
 
 // mount routes
 
-app.get('/', function(req, res) {
-  res.send('<h1>hello, james!</h1>')
-})
+// app.get('/', function(req, res) {
+//   res.send('<h1>hello, james!</h1>')
+// })
 
 app.get('/home', function(req, res) {
   res.render('home')
+})
+
+app.get('/formula1', function(req, res) {
+  res.render('forumla1/index', {
+    formula1: formula1
+  })
 })
 
 // tell teh app to listen on port 3000
